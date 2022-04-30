f7f6cda7a87682b10d35d5116f69ed2252ef2f8a
jamesprenticez <jamesprenticez@gmail.com>
2022-04-30 14:24:19 +1200

    Movement

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ class Sprite {
   update(){
     this.draw()
 
+    this.position.x +=  this.velocity.x
     this.position.y +=  this.velocity.y
     
     if(this.position.y + this.height + this.velocity.y >= canvasY){
@@ -57,12 +58,68 @@ const enemy = new Sprite({
   }
 })
 
+//fixes keydown bug
+const keys = {
+  w: {pressed: false},
+  a: {pressed: false},
+  s: {pressed: false},
+  d: {pressed: false},
+}
+
+let lastKey
+
 function animate(){
   window.requestAnimationFrame(animate)
   ctx.fillStyle = "black"
   ctx.fillRect(0, 0, canvasX, canvasY)
   player.update()
   enemy.update()
+
+  if(keys.w.pressed){
+    player.velocity.y =- 10
+  } else if (keys.a.pressed && lastKey === "a") {
+    player.velocity.x = -1
+  } else if (keys.s.pressed) {
+    //dosomthing
+  } else if (keys.d.pressed && lastKey === "d") {
+    player.velocity.x = 1
+  }
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'w':
+      keys.w.pressed = true
+    break
+    case 'a':
+      keys.a.pressed = true
+      lastKey= 'a'
+    break
+    case 's':
+      keys.s.pressed = true
+    break
+    case 'd': 
+      keys.d.pressed = true
+      lastKey= 'd'
+    break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'w':
+      keys.w.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+    case 's':
+      keys.s.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+  }
+})
